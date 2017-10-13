a672ad4b6026e250e41d71bc09c6c633de72188a
Markus Rudy <webmaster@burgerdev.de>
2017-10-14 00:49:59 +0200

    reworked docker build script

--- docker-build.sh
+++ docker-build.sh
@@ -2,39 +2,74 @@
 
 set -ex
 
+# determine final docker tag
+
 if [[ -n "$1" ]]
 then
     file_variant=".$1"
-    tag_variant="-$1"
 else
     file_variant=""
-    tag_variant=""
 fi
+
+patch=""
+for extra in "$@"
+do
+    if [[ -n "$extra" ]]
+    then
+        patch="$patch-$extra"
+    fi
+done
+
+version="`git describe --abbrev=0 --tags`"
+
+oam_repo="${OAM_REPO:-burgerdev/}"
+oam_project="${OAM_PROJECT:-ocaml-addressmapper}"
+oam_project_devel="${OAM_PROJECT_DEVEL:-ocaml-addressmapper-devel}"
+
+function prepare_image {
+    imagedir=".circleci/images/$1"
+    image="${oam_repo}$1:$version$patch"
+}
+
+function build_image {
+    docker build -t $image -f $imagedir/Dockerfile$file_variant $imagedir
+}
+
+# build devel image first, compile main.native from there
+
+prepare_image ocaml-addressmapper-devel
+build_image ocaml-addressmapper-devel
+
+name="xxx-tmp-devel-container-$$"
+
+contdir="/home/opam/project"
+volume="$(pwd):$contdir:ro"
+
+function docker_make {
+    prog="cp -r $contdir ${contdir}2
+          cd ${contdir}2
+          git clean -dfx
+          make $@"
+}
+
+if [[ -n "$OAM_TEST" ]]
+then
+    docker_make clean test
+    docker run --name "$name" -v "$volume" $image /bin/sh -c "$prog"
+    docker rm "$name"
+fi
+
+docker_make clean build
+
+container=$(docker run -d --name "$name" -v $volume $image /bin/sh -c "$prog")
+docker wait $container
+
+prepare_image ocaml-addressmapper
 
 build_dir=.build
 rm -rf $build_dir
-
-version=`git describe --abbrev=0 --tags`
-
-tag=burgerdev/ocaml-addressmapper-devel:$version$tag_variant
-
-pushd .circleci/images/ocaml-addressmapper-devel
-docker build -t $tag -f Dockerfile$file_variant .
-popd
-
-container=$(docker run -d -v $(pwd):/home/opam/project:ro $tag /bin/sh -c "cp -r /home/opam/project /home/opam/project2; cd /home/opam/project2; git clean -df; make all")
-
-docker wait $container
-
 docker cp $container:/home/opam/project2/_build $build_dir
-
-cp $build_dir/src/main.native .circleci/images/ocaml-addressmapper/
-
-prodtag=burgerdev/ocaml-addressmapper:$version$tag_variant
-
-pushd .circleci/images/ocaml-addressmapper
-docker build -t $prodtag -f Dockerfile$file_variant .
-popd
-
+cp $build_dir/src/main.native $imagedir/
 docker rm $container
 
+build_image ocaml-addressmapper
