1594db48de218732cecfabfffab8267f61d395c9
Markus Rudy <markus.rudy@sap.com>
2017-11-01 20:57:02 +0100

    remove cleanup functionality for init process
    Justification: KISS / questionable benefit
    
    closes #19

--- src/init.ml
+++ src/init.ml
@@ -1,6 +1,7 @@
 open Unix
 open Sys
 
+(* Why is this not part of Unix? *)
 let string_of_signal = function
   | i when i = sigint -> "SIGINT"
   | i when i = sigterm -> "SIGTERM"
@@ -11,55 +12,8 @@ let string_of_signal = function
   | i when i = sigfpe -> "SIGFPE"
   | i -> string_of_int i
 
-type cleanup =
-  | Kill_group
-  | Kill_parent_group
-  | Kill_pid of int
-
-(* TODO implement safe signalling (catch ESRCH) *)
-let safe_kill pid signo =
-  try
-    kill pid signo
-  with
-  | Unix_error (ESRCH, _, _) ->
-    (* child does not exist anymore, ignore *)
-    ()
-
-let graceful_kill whom () =
-  (* start with issuing SIGTERM (we might be a recipient, so ignore) *)
-  set_signal sigterm Signal_ignore;
-  safe_kill whom sigterm;
-
-  (* SIGKILL after grace period *)
-  let on_alarm _ =
-    try
-      Logs.warn (fun m -> m "Could not kill all children.");
-      safe_kill whom sigkill;
-      (* TODO if we were not part of the killed processes *)
-      (* safe_kill 0 sigkill ??? *)
-    with
-    | _ ->
-      (* ignore cleanup errors *)
-      Logs.err (fun m -> m "Errors occured during cleanup.");
-      ()
-  in set_signal sigalrm (Signal_handle on_alarm);
-
-  (* schedule SIGKILL *)
-  ignore (alarm 5);
-
-  (* keep reaping while the countdown is running *)
-  let rec aux _ =
-    try
-      ignore (wait ())
-    with
-    | Unix_error (ECHILD, _, _) ->
-      (* all children dead, abort kill and return normally *)
-      set_signal sigalrm Signal_ignore
-    | Unix_error (EINTR, _, _) -> aux ()
-  in aux ()
-
 (* wait for any child to terminate, exit if process 'n' *)
-let rec supervise_pid cleanup n =
+let rec supervise_pid n =
   try
     match wait () with
     | (m, cause) when m = n ->
@@ -67,40 +21,42 @@ let rec supervise_pid cleanup n =
         match cause with
         | WEXITED status_code ->
           Logs.info (fun m -> m "Child exited with [%d]." status_code);
-          cleanup ();
           cause
         | WSIGNALED signo ->
           Logs.info (fun m -> m "Child terminated with signal [%s]." (string_of_signal signo));
-          cleanup ();
           cause
         | _ ->
           (* ignore stopped *)
-          supervise_pid cleanup n
-      end;
+          supervise_pid n
+      end
     | _ ->
       (* some other child exited, we reap it and continue *)
-      supervise_pid cleanup n
+      supervise_pid n
   with
   | Unix_error (EINTR, _, _) ->
     (* our wait got interrupted, restart it *)
-    supervise_pid cleanup n
+    supervise_pid n
 
-let supervise ?cleanup:(cleanup=None) f =
+let deliver_signal to_whom signo =
+  let p = getpid () in
+  let s = string_of_signal signo in
+  Logs.debug (fun m -> m "[%d]: Forwarding signal [%s] to [%d]." p s to_whom);
+  try kill to_whom signo with
+  | Unix_error (ESRCH, _, _) ->
+    Logs.warn (fun m -> m "[%d]: Could not deliver signal [%s] to lost child [%d]." p s to_whom)
+
+let supervise f =
   match fork () with
-    | 0 -> f (); exit 0
-    | n ->
-      Logs.info (fun m -> m "[%d]: Supervising [%d]." (getpid ()) n);
-      let handler signo =
-        Logs.debug (fun m -> m "[%d]: Forwarding signal [%s] to [%d]." (getpid ()) (string_of_signal signo) n);
-        safe_kill n signo
-      in
-      set_signal sigint (Signal_handle handler);
-      set_signal sigterm (Signal_handle handler);
-      let cleanup_fun =
-        match cleanup with
-        | Some (Kill_pid pid) -> graceful_kill pid
-        | Some Kill_group -> graceful_kill (-n)
-        | Some Kill_parent_group -> graceful_kill (- getpid ())
-        | None -> fun _ -> ()
-      in
-      supervise_pid cleanup_fun n
+  | 0 ->
+    (* this is the child to supervise *)
+    f ();
+    (* if we get to this point, the child finished it's job and we return the
+       traditional 0 to indicate this to the caller. *)
+    exit 0
+  | n ->
+    Logs.info (fun m -> m "[%d]: Supervising [%d]." (getpid ()) n);
+    let handler = deliver_signal n in
+    set_signal sigint (Signal_handle handler);
+    set_signal sigterm (Signal_handle handler);
+    set_signal sigquit (Signal_handle handler);
+    supervise_pid n
--- src/init.mli
+++ src/init.mli
@@ -1,13 +1,7 @@
 
-type cleanup =
-  | Kill_group
-  | Kill_parent_group
-  | Kill_pid of int
-
-val supervise:
-  ?cleanup:cleanup option -> (unit -> unit) -> Unix.process_status
-(* [supervise c f] runs the given function f in a forked process and returns
-   this process' return value. After the main child exited, leftover processes
-   can be cleaned up by killing the main child's group, the supervisor's group
-   or a specific PID (you might want to use '-1' if running in a container).
+val supervise: (unit -> unit) -> Unix.process_status
+(* [supervise f] runs the given function f in a forked process and returns
+   this process' exit status. The supervising process waits on all children that
+   get assigned to it, thus reaping zombies caused by double-forks (e.g.
+   [Unix.establish_server]) when running as pid 1 in a container.
 *)
