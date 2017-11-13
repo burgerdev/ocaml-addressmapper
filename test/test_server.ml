60b9d0fbc1d48a0889922a756a2166950de4c3b6
Markus Rudy <markus.rudy@sap.com>
2017-11-13 16:17:15 +0100

    reworked request parsing
    - new server module that handles request/response
    - new request type "health"
    
    closes #30

---
+++ test/test_server.ml
@@ -0,0 +1,19 @@
+open OUnit
+open Server
+
+let must_match pattern = function
+  | x when pattern = x -> ()
+  | x -> failwith ("did not match [" ^ (string_of_request x) ^ "]")
+
+let test_parse _ =
+  must_match (Get "abuse@example.com") (request_of_string "get abuse@example.com");
+  must_match (Put "abuse@example.com") (request_of_string "put abuse@example.com");
+  must_match Health (request_of_string "health");
+  must_match (Invalid "something else") (request_of_string "something else")
+
+let suite =
+  "mapper suite" >::: [ "test_parse" >:: test_parse
+                      ]
+
+let _ =
+  run_test_tt_main suite
