c1f83b864183c369af278a5ca9c614b879900424
Markus Rudy <markus.rudy@sap.com>
2018-01-01 14:26:32 +0100

    make init wrapper optional
    (closes #40)

--- lib/init.mli
+++ lib/init.mli
@@ -4,3 +4,9 @@ val supervise: (unit -> unit) -> Unix.process_status
     this process' exit status. The supervising process waits on all children that
     get assigned to it, thus reaping zombies caused by double-forks (e.g.
     [Unix.establish_server]) when running as pid 1 in a container. *)
+
+val string_of_signal: int -> string
+(** [string_of_signal s] tries to convert the OCaml signal number [s] to its
+    canonical string representation, e.g. SIGTERM. Only a small subset of the
+    available signals is actually implemented, all others are converted using
+    [string_of_int]. *)
