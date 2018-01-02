f8a0694808419228dfb914f7dada67e06c880721
Markus Rudy <markus.rudy@sap.com>
2018-01-02 21:03:53 +0100

    add debug messages for predefined rules
    fixes #44

--- lib/mapper_predef.ml
+++ lib/mapper_predef.ml
@@ -2,6 +2,33 @@
 
 open Mapper_rule
 
+let log_src = Logs.Src.create "rule-mapper-predef"
+
+module Logs = (val (Logs.src_log log_src))
+
+let pp_opt ppf = function
+  | Some s -> Fmt.pf ppf "Some(%s)" s
+  | None -> Fmt.pf ppf "None"
+
+let pp_comp ppf (input, output) =
+  Fmt.(pf ppf "%s -> %a" input pp_opt output)
+
+let log_wrap pp f = fun input ->
+  let output = f input in
+  Logs.debug (fun m -> m "%a: %a" pp () pp_comp (input, output));
+  output
+
+let create name f = create name @@ log_wrap Fmt.(const string name) f
+let create_pp pp f =
+  let name = match pp with
+    | Some pp -> pp
+    | None -> Fmt.(const string "unknown")
+  in
+  create_pp pp @@ log_wrap name f
+
+let accept = create "accept" @@ fun x -> Some x
+let reject = create "reject" @@ fun x -> None
+
 let lower = create "lower" @@ fun input -> Some (String.lowercase_ascii input)
 let upper = create "upper" @@ fun input -> Some (String.uppercase_ascii input)
 
@@ -59,10 +86,10 @@ let prefix_matches pattern =
 let suffix_matches pattern =
   let n = String.length pattern in
   let f input =
-  let m = String.length input in
-  if m >= n && String.sub input (m-n) n = pattern then
-    Some input
-  else
-    None
+    let m = String.length input in
+    if m >= n && String.sub input (m-n) n = pattern then
+      Some input
+    else
+      None
   in
   create_pp (pp_unary "suffix_matches" pattern) f
